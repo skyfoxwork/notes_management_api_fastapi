ace53ef77829ef5353e68588bd87576bd13191b7
roman <skyfoxwork@gmail.com>
2025-03-17 19:33:01 +0200

    add config package with settings.py with app settings

---
+++ src/config/settings.py
@@ -0,0 +1,14 @@
+import os
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+class Settings:
+    POSTGRES_USER: str = os.getenv("POSTGRES_USER", "test_user")
+    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD", "test_password")
+    POSTGRES_HOST: str = os.getenv("POSTGRES_HOST", "test_host")
+    POSTGRES_DB_PORT: int = int(os.getenv("POSTGRES_DB_PORT", 5432))
+    POSTGRES_DB: str = os.getenv("POSTGRES_DB_NAME", "test_db")
