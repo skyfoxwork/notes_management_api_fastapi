72fbd824298cd9d3d280d4bc18a74af486befd80
roman <skyfoxwork@gmail.com>
2025-03-17 20:19:50 +0200

    fix session_postgresql.py change async_sessionmaker

--- src/database/session_postgresql.py
+++ src/database/session_postgresql.py
@@ -1,5 +1,4 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 
 from src.config.dependencies import get_settings
 
@@ -8,8 +7,10 @@ settings = get_settings()
 
 POSTGRESQL_DATABASE_URL = (f"postgresql+asyncpg://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@"
                            f"{settings.POSTGRES_HOST}:{settings.POSTGRES_DB_PORT}/{settings.POSTGRES_DB}")
+
 postgresql_engine = create_async_engine(POSTGRESQL_DATABASE_URL, echo=True, future=True)
-PostgresqlSessionLocal = sessionmaker(
+
+AsyncPostgresqlSessionLocal = async_sessionmaker(
     postgresql_engine,
     class_=AsyncSession,
     expire_on_commit=False,
@@ -20,5 +21,5 @@ PostgresqlSessionLocal = sessionmaker(
 
 
 async def get_db():
-    async with PostgresqlSessionLocal() as session:
-        yield session
+    async with AsyncPostgresqlSessionLocal() as async_session:
+        yield async_session
