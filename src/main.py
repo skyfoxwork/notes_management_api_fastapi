e1e149bcafaa869ebe707ddbd4f99e7a19de3de2
roman <skyfoxwork@gmail.com>
2025-03-17 19:53:00 +0200

    add notes route to src/main.py module

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
 
+from src.routes.notes import router as notes_router
+
 
 app = FastAPI(title="Notes Management API", description="Project description")
 
 
-@app.get("/")
-async def main():
-    return {"message": "ok"}
+api_version_prefix = "/api/v1"
+
+app.include_router(
+    notes_router, prefix=f"{api_version_prefix}/notes", tags=["notes"]
+)
