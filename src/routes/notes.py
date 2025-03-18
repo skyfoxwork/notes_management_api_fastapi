eb166f11991bcb6536985d9369974e53b8ae2329
roman <skyfoxwork@gmail.com>
2025-03-18 17:53:58 +0200

    add update_note router to routes/notes.py and fix routers

--- src/routes/notes.py
+++ src/routes/notes.py
@@ -2,11 +2,13 @@ import uuid
 
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
+from sqlalchemy import select, delete, func
+from sqlalchemy.orm import selectinload
+from sqlalchemy.sql.functions import coalesce
 
 from src.database.session_postgresql import get_db
-from src.database.models.notes import Note
-from src.schemas.notes import NoteCreateSchema, NoteSchema, NoteSchemaDetail
+from src.database.models.notes import Note, NoteVersion
+from src.schemas.notes import NoteCreateSchema, NoteSchema, NoteSchemaDetail, NoteUpdateCreateVersionSchema
 
 
 router = APIRouter()
@@ -24,9 +26,13 @@ async def get_list_of_notes(db: AsyncSession = Depends(get_db)):
 
 
 @router.get("/{note_id}/", response_model=NoteSchemaDetail, status_code=status.HTTP_200_OK)
-async def get_note_by_id(note_id: uuid.UUID , db: AsyncSession = Depends(get_db)):
-    db_result = await db.execute(select(Note).filter(Note.id == note_id))
-    note = db_result.scalar_one_or_none()
+async def get_note_by_id(note_id: uuid.UUID, db: AsyncSession = Depends(get_db)):
+    db_result = await db.execute(
+        select(Note)
+        .options(selectinload(Note.versions))
+        .filter(Note.id == note_id)
+    )
+    note = db_result.scalars().first()
 
     if not note:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not found")
@@ -41,3 +47,45 @@ async def create_note(note: NoteCreateSchema, db: AsyncSession = Depends(get_db)
     await db.commit()
     await db.refresh(new_note)
     return new_note
+
+
+@router.delete("/{note_id}/", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_note(note_id: uuid.UUID, db: AsyncSession = Depends(get_db)):
+    result = await db.execute(delete(Note).where(Note.id == note_id))
+    if result.rowcount == 0:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Note with the given UUID was not found."
+        )
+    await db.commit()
+
+
+@router.patch("/{note_id}/", response_model=NoteSchema, status_code=status.HTTP_200_OK)
+async def update_note(data: NoteUpdateCreateVersionSchema, note_id: uuid.UUID, db: AsyncSession = Depends(get_db)):
+    note = await db.get(Note, note_id)
+
+    if not note:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not found")
+
+    if note.content == data.content:
+        return {"detail": "content not changed"}
+
+    version_number = (await db.execute(
+        select(coalesce(func.max(NoteVersion.version), 0))
+        .filter(NoteVersion.note_id == note_id)
+    )).scalar()
+
+    new_version = NoteVersion(
+        note_id=note.id,
+        version=version_number + 1,
+        content=note.content
+    )
+
+    db.add(new_version)
+
+    note.content = data.content
+
+    await db.commit()
+    await db.refresh(note)
+
+    return note
