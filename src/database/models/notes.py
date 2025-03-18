ceb7d6fb3fa30705a876e9173e38c1381325bed6
roman <skyfoxwork@gmail.com>
2025-03-18 17:52:57 +0200

    delete imports to NoteVersion model

--- src/database/models/notes.py
+++ src/database/models/notes.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import String, Text, DateTime, ForeignKey, Integer
+from sqlalchemy import String, Text, DateTime, ForeignKey
 
 from src.database.models.base import Base
 
