49870473e03f8d4ff9c317af27ad0fd78bf41f19
roman <skyfoxwork@gmail.com>
2025-03-18 17:52:29 +0200

    add version field to NoteVersion model

--- src/database/models/notes.py
+++ src/database/models/notes.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import String, Text, DateTime, ForeignKey
+from sqlalchemy import String, Text, DateTime, ForeignKey, Integer
 
 from src.database.models.base import Base
 
@@ -21,12 +21,20 @@ class Note(Base):
         back_populates="note", cascade="all, delete-orphan"
     )
 
+    def __repr__(self):
+        return f"id: {self.id}, title: {self.title}"
+
+
 class NoteVersion(Base):
     __tablename__ = "note_versions"
 
     id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     note_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), ForeignKey("notes.id", ondelete="CASCADE"), nullable=False)
+    version: Mapped[int] = mapped_column(nullable=False)
     content: Mapped[str] = mapped_column(Text, nullable=False)
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
 
     note: Mapped["Note"] = relationship(back_populates="versions")
+
+    def __repr__(self):
+        return f"id: {self.id}, note_id: {self.note_id} version: {self.version}"
