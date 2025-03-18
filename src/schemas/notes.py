662a1385936bd1d02405725c408c345707b68a57
roman <skyfoxwork@gmail.com>
2025-03-18 17:56:41 +0200

    add NoteUpdateCreateVersionSchema to schemas/notes.py

--- src/schemas/notes.py
+++ src/schemas/notes.py
@@ -1,5 +1,6 @@
 import uuid
 from datetime import datetime
+from typing import List
 
 from pydantic import BaseModel
 
@@ -13,10 +14,22 @@ class NoteSchema(NoteBaseSchema):
     id: uuid.UUID
 
 
+class VersionsSchema(BaseModel):
+    id: uuid.UUID
+    version: int
+    content: str
+    created_at: datetime
+
+
 class NoteSchemaDetail(NoteSchema):
     created_at: datetime
     updated_at: datetime
+    versions: List[VersionsSchema]
 
 
 class NoteCreateSchema(NoteBaseSchema):
     pass
+
+
+class NoteUpdateCreateVersionSchema(BaseModel):
+    content: str
