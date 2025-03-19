0d0754c330f2f7b8c2c8810cfa073600803cebc9
roman <skyfoxwork@gmail.com>
2025-03-19 15:02:42 +0200

    add router for analytics to routers package

---
+++ src/routes/analytics.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+
+from src.database.session_postgresql import get_db
+from src.database.models.notes import Note
+from src.services.analytics import get_analyze_notes
+
+
+router =  APIRouter()
+
+
+@router.get("/")
+async def get_notes_analytics(db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(Note))
+    notes = result.scalars().all()
+
+    if not notes:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not found")
+
+    analyze_data = await get_analyze_notes(notes)
+
+    return analyze_data
