d13eb4c421f5dd71c4baefd943d9bce4492ce457
roman <skyfoxwork@gmail.com>
2025-03-23 12:03:23 +0200

    add accounts end points for users to src/routes/accounts.py

---
+++ src/routes/accounts.py
@@ -0,0 +1,280 @@
+from typing import cast
+
+from fastapi import APIRouter, Depends, status, HTTPException
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+
+from src.config.dependencies import (
+    get_jwt_auth_manager,
+    get_settings,
+    Settings
+)
+from src.database.session_postgresql import get_db
+from src.database.models.accounts import (
+    UserModel,
+    UserGroupModel,
+    UserGroupEnum,
+    RefreshTokenModel
+)
+from src.exceptions.security import BaseSecurityError
+
+
+from src.schemas.accounts import (
+    UserRegistrationRequestSchema,
+    UserRegistrationResponseSchema,
+    UserLoginResponseSchema,
+    UserLoginRequestSchema,
+    TokenRefreshRequestSchema,
+    TokenRefreshResponseSchema
+)
+from src.security.interfaces import JWTAuthManagerInterface
+
+router = APIRouter()
+
+
+@router.post(
+    "/register/",
+    response_model=UserRegistrationResponseSchema,
+    summary="User Registration",
+    description="Register a new user with an email and password.",
+    status_code=status.HTTP_201_CREATED,
+    responses={
+        409: {
+            "description": "Conflict - User with this email already exists.",
+            "content": {
+                "application/json": {
+                    "example": {
+                        "detail": "A user with this email test@example.com already exists."
+                    }
+                }
+            },
+        },
+        500: {
+            "description": "Internal Server Error - An error occurred during user creation.",
+            "content": {
+                "application/json": {
+                    "example": {
+                        "detail": "An error occurred during user creation."
+                    }
+                }
+            },
+        },
+    }
+)
+async def register_user(
+        user_data: UserRegistrationRequestSchema,
+        db: AsyncSession = Depends(get_db),
+) -> UserRegistrationResponseSchema:
+    """
+    Endpoint for user registration.
+
+    Registers a new user, hashes their password, and assigns them to the default user group.
+    If a user with the same email already exists, an HTTP 409 error is raised.
+    In case of any unexpected issues during the creation process, an HTTP 500 error is returned.
+    """
+    db_result_user = await db.execute(select(UserModel).filter_by(email=user_data.email))
+    existing_user = db_result_user.scalars().first()
+
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"A user with this email {user_data.email} already exists."
+        )
+
+    db_result_group = await db.execute(select(UserGroupModel).filter_by(name=UserGroupEnum.USER))
+    user_group = db_result_group.scalars().first()
+
+    if not user_group:
+        user_group = UserGroupModel(name=UserGroupEnum.USER)
+        db.add(user_group)
+        await db.flush()
+        await db.refresh(user_group)
+
+    try:
+        new_user = UserModel.create(
+            email=str(user_data.email),
+            raw_password=user_data.password,
+            group_id=user_group.id,
+        )
+        db.add(new_user)
+        await db.flush()
+
+        await db.commit()
+        await db.refresh(new_user)
+    except SQLAlchemyError:
+        await db.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="An error occurred during user creation."
+        )
+
+    return UserRegistrationResponseSchema.model_validate(new_user)
+
+
+@router.post(
+    "/login/",
+    response_model=UserLoginResponseSchema,
+    summary="User Login",
+    description="Authenticate a user and return access and refresh tokens.",
+    status_code=status.HTTP_201_CREATED,
+    responses={
+        401: {
+            "description": "Unauthorized - Invalid email or password.",
+            "content": {
+                "application/json": {
+                    "example": {
+                        "detail": "Invalid email or password."
+                    }
+                }
+            },
+        },
+        403: {
+            "description": "Forbidden - User account is not activated.",
+            "content": {
+                "application/json": {
+                    "example": {
+                        "detail": "User account is not activated."
+                    }
+                }
+            },
+        },
+        500: {
+            "description": "Internal Server Error - An error occurred while processing the request.",
+            "content": {
+                "application/json": {
+                    "example": {
+                        "detail": "An error occurred while processing the request."
+                    }
+                }
+            },
+        },
+    },
+)
+async def login_user(
+        login_data: UserLoginRequestSchema,
+        db: AsyncSession = Depends(get_db),
+        settings: Settings = Depends(get_settings),
+        jwt_manager: JWTAuthManagerInterface = Depends(get_jwt_auth_manager),
+) -> UserLoginResponseSchema:
+    """
+    Endpoint for user login.
+
+    Authenticates a user using their email and password.
+    If authentication is successful, creates a new refresh token and
+    returns both access and refresh tokens.
+    """
+    db_result_user = await db.execute(select(UserModel).filter_by(email=login_data.email))
+    user = cast(UserModel, db_result_user.scalars().first())
+
+    if not user or not user.verify_password(login_data.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid email or password.",
+        )
+
+    jwt_refresh_token = jwt_manager.create_refresh_token({"user_id": user.id})
+
+    try:
+        refresh_token = RefreshTokenModel.create(
+            user_id=user.id,
+            days_valid=settings.LOGIN_TIME_DAYS,
+            token=jwt_refresh_token
+        )
+        db.add(refresh_token)
+        await db.flush()
+        await db.commit()
+    except SQLAlchemyError:
+        await db.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="An error occurred while processing the request.",
+        )
+
+    jwt_access_token = jwt_manager.create_access_token({"user_id": user.id})
+    return UserLoginResponseSchema(
+        access_token=jwt_access_token,
+        refresh_token=jwt_refresh_token,
+    )
+
+
+@router.post(
+    "/refresh/",
+    response_model=TokenRefreshResponseSchema,
+    summary="Refresh Access Token",
+    description="Refresh the access token using a valid refresh token.",
+    status_code=status.HTTP_200_OK,
+    responses={
+        400: {
+            "description": "Bad Request - The provided refresh token is invalid or expired.",
+            "content": {
+                "application/json": {
+                    "example": {
+                        "detail": "Token has expired."
+                    }
+                }
+            },
+        },
+        401: {
+            "description": "Unauthorized - Refresh token not found.",
+            "content": {
+                "application/json": {
+                    "example": {
+                        "detail": "Refresh token not found."
+                    }
+                }
+            },
+        },
+        404: {
+            "description": "Not Found - The user associated with the token does not exist.",
+            "content": {
+                "application/json": {
+                    "example": {
+                        "detail": "User not found."
+                    }
+                }
+            },
+        },
+    },
+)
+async def refresh_access_token(
+        token_data: TokenRefreshRequestSchema,
+        db: AsyncSession = Depends(get_db),
+        jwt_manager: JWTAuthManagerInterface = Depends(get_jwt_auth_manager),
+) -> TokenRefreshResponseSchema:
+    """
+    Endpoint to refresh an access token.
+
+    Validates the provided refresh token, extracts the user ID from it, and issues
+    a new access token. If the token is invalid or expired, an error is returned.
+    """
+    try:
+        decoded_token = jwt_manager.decode_refresh_token(token_data.refresh_token)
+        user_id = decoded_token.get("user_id")
+    except BaseSecurityError as error:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=str(error),
+        )
+
+    db_result_refresh_token_record = await db.execute(select(RefreshTokenModel).filter_by(token=token_data.refresh_token))
+    refresh_token_record = db_result_refresh_token_record.scalars().first()
+
+    if not refresh_token_record:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Refresh token not found.",
+        )
+
+    db_result_user = await db.execute(select(UserModel).filter_by(id=user_id))
+    user = db_result_user.scalars().first()
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found.",
+        )
+
+    new_access_token = jwt_manager.create_access_token({"user_id": user_id})
+
+    return TokenRefreshResponseSchema(access_token=new_access_token)
