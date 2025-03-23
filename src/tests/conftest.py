a206559d7ac23b7468d819b17d185d66ef48b403
roman <skyfoxwork@gmail.com>
2025-03-23 12:11:32 +0200

    add tests configurations

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,10 +1,18 @@
 import pytest_asyncio
 from httpx import AsyncClient, ASGITransport
+from src.config.settings import Settings
 
 from src.main import app
 from src.database.session_sqlite import init_db, get_sqlite_db_contextmanager
 from src.database.session_postgresql import get_db
 from src.database.session_sqlite import get_sqlite_db
+from src.security.token_manager import JWTAuthManager
+from src.config.dependencies import get_settings
+
+
+@pytest_asyncio.fixture(scope="session")
+async def settings():
+    return get_settings()
 
 
 @pytest_asyncio.fixture
@@ -25,3 +33,19 @@ async def setup_db():
 async def db_session():
     async with get_sqlite_db_contextmanager() as session:
         yield session
+
+
+@pytest_asyncio.fixture(scope="function")
+async def jwt_manager(settings: Settings):
+    return JWTAuthManager(
+        secret_key_access=settings.SECRET_KEY_ACCESS,
+        secret_key_refresh=settings.SECRET_KEY_REFRESH,
+        algorithm=settings.JWT_SIGNING_ALGORITHM
+    )
+
+
+from sqlalchemy import text
+@pytest_asyncio.fixture(autouse=True)
+async def enable_foreign_keys(db_session):
+    await db_session.execute(text("PRAGMA foreign_keys = ON;"))
+    await db_session.commit()
