dc8cd34b23447087e10d9a0e6cc8212a1c4961e0
roman <skyfoxwork@gmail.com>
2025-03-23 11:34:28 +0200

    add get_jwt_auth_manager to src/config/dependencies.py

--- src/config/dependencies.py
+++ src/config/dependencies.py
@@ -1,5 +1,17 @@
+from fastapi import Depends
+
 from src.config.settings import Settings
+from src.security.token_manager import JWTAuthManager
+from src.security.interfaces import JWTAuthManagerInterface
 
 
 def get_settings() -> Settings:
     return Settings()
+
+
+def get_jwt_auth_manager(settings: Settings = Depends(get_settings)) -> JWTAuthManagerInterface:
+    return JWTAuthManager(
+        secret_key_access=settings.SECRET_KEY_ACCESS,
+        secret_key_refresh=settings.SECRET_KEY_REFRESH,
+        algorithm=settings.JWT_SIGNING_ALGORITHM
+    )
