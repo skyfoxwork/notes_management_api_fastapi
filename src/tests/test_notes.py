e73cd8d5298e84e9cd3e89f55483aba5af7fe100
roman <skyfoxwork@gmail.com>
2025-03-23 17:24:46 +0200

    fix test_notes.py

--- src/tests/test_notes.py
+++ src/tests/test_notes.py
@@ -5,43 +5,9 @@ import pytest
 from httpx import AsyncClient
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.database.models.accounts import (
-    UserModel,
-    UserGroupModel,
-    UserGroupEnum
-)
-from src.database.models.notes import Note, NoteVersion
+from src.database.models.notes import Note
 from src.security.token_manager import JWTAuthManager
 from src.tests.utils import create_user, generate_token
-
-
-
-def generate_token(user_id, jwt_manager):
-    """
-    Generates an access token for a user.
-    """
-    return jwt_manager.create_access_token({"user_id": user_id})
-
-
-async def create_user(db_session: AsyncSession):
-    user_group = UserGroupModel(name=UserGroupEnum.USER)
-    db_session.add(user_group)
-
-    await db_session.commit()
-    await db_session.refresh(user_group)
-
-    user = UserModel.create(
-        email="test@test.com",
-        raw_password="Test_password12345@",
-        group_id=user_group.id,
-    )
-
-    db_session.add(user)
-
-    await db_session.commit()
-    await db_session.refresh(user)
-
-    return user
 
 
 @pytest.mark.asyncio
