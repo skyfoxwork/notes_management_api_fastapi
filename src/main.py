de077b6f39c1337db70a7dac408339750fbb3d09
roman <skyfoxwork@gmail.com>
2025-03-23 12:02:12 +0200

    add include router for accounts

--- src/main.py
+++ src/main.py
@@ -3,12 +3,15 @@ from fastapi import FastAPI
 from src.routes.notes import router as notes_router
 from src.routes.ai import router as ai_router
 from src.routes.analytics import router as analytics_router
-
+from src.routes.accounts import router as accounts_router
 
 app = FastAPI(title="Notes Management API", description="Project description")
 
 api_version_prefix = "/api/v1"
 
+app.include_router(
+    accounts_router, prefix=f"{api_version_prefix}/accounts", tags=["accounts"]
+)
 app.include_router(
     notes_router, prefix=f"{api_version_prefix}/notes", tags=["notes"]
 )
