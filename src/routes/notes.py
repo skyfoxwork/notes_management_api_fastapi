fec66d927d403bc40768b7ad66b316704df2d087
roman <skyfoxwork@gmail.com>
2025-03-23 12:04:28 +0200

    add authorization for users to notes endpoints

--- src/routes/notes.py
+++ src/routes/notes.py
@@ -9,14 +9,24 @@ from sqlalchemy.sql.functions import coalesce
 from src.database.session_postgresql import get_db
 from src.database.models.notes import Note, NoteVersion
 from src.schemas.notes import NoteCreateSchema, NoteSchema, NoteSchemaDetail, NoteUpdateVersionCreateSchema
+from src.security.token_manager import JWTAuthManagerInterface
+from src.config.dependencies import get_jwt_auth_manager
+from src.security.http import get_token
+from src.services.accounts import get_user_id_or_unauthorized
 
 
 router = APIRouter()
 
 
 @router.get("/", response_model=list[NoteSchema], status_code=status.HTTP_200_OK)
-async def get_list_of_notes(db: AsyncSession = Depends(get_db)):
-    db_result = await db.execute(select(Note))
+async def get_list_of_notes(
+        jwt_manager: JWTAuthManagerInterface = Depends(get_jwt_auth_manager),
+        token: str = Depends(get_token),
+        db: AsyncSession = Depends(get_db),
+):
+    user_id = get_user_id_or_unauthorized(jwt_manager=jwt_manager, token=token)
+
+    db_result = await db.execute(select(Note).filter(Note.user_id == user_id))
     notes = db_result.scalars().all()
 
     if not notes:
@@ -26,11 +36,18 @@ async def get_list_of_notes(db: AsyncSession = Depends(get_db)):
 
 
 @router.get("/{note_id}/", response_model=NoteSchemaDetail, status_code=status.HTTP_200_OK)
-async def get_note_by_id(note_id: uuid.UUID, db: AsyncSession = Depends(get_db)):
+async def get_note_by_id(
+        note_id: uuid.UUID,
+        db: AsyncSession = Depends(get_db),
+        jwt_manager: JWTAuthManagerInterface = Depends(get_jwt_auth_manager),
+        token: str = Depends(get_token),
+):
+    user_id = get_user_id_or_unauthorized(jwt_manager=jwt_manager, token=token)
+
     db_result = await db.execute(
         select(Note)
         .options(selectinload(Note.versions))
-        .filter(Note.id == note_id)
+        .filter(Note.id == note_id, Note.user_id == user_id)
     )
     note = db_result.scalars().first()
 
@@ -41,8 +58,14 @@ async def get_note_by_id(note_id: uuid.UUID, db: AsyncSession = Depends(get_db))
 
 
 @router.post("/", response_model=NoteSchema, status_code=status.HTTP_201_CREATED)
-async def create_note(note: NoteCreateSchema, db: AsyncSession = Depends(get_db)):
-    new_note = Note(title=note.title, content=note.content)
+async def create_note(
+        note: NoteCreateSchema,
+        db: AsyncSession = Depends(get_db),
+        token: str = Depends(get_token),
+        jwt_manager: JWTAuthManagerInterface = Depends(get_jwt_auth_manager)
+):
+    user_id = get_user_id_or_unauthorized(jwt_manager=jwt_manager, token=token)
+    new_note = Note(title=note.title, content=note.content, user_id=user_id)
     db.add(new_note)
     await db.commit()
     await db.refresh(new_note)
@@ -50,8 +73,15 @@ async def create_note(note: NoteCreateSchema, db: AsyncSession = Depends(get_db)
 
 
 @router.delete("/{note_id}/", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_note(note_id: uuid.UUID, db: AsyncSession = Depends(get_db)):
-    result = await db.execute(delete(Note).where(Note.id == note_id))
+async def delete_note(
+        note_id: uuid.UUID,
+        db: AsyncSession = Depends(get_db),
+        token: str = Depends(get_token),
+        jwt_manager: JWTAuthManagerInterface = Depends(get_jwt_auth_manager)
+):
+    user_id = get_user_id_or_unauthorized(jwt_manager=jwt_manager, token=token)
+
+    result = await db.execute(delete(Note).where(Note.id == note_id, Note.user_id == user_id))
 
     if result.rowcount == 0:
         raise HTTPException(
@@ -63,7 +93,15 @@ async def delete_note(note_id: uuid.UUID, db: AsyncSession = Depends(get_db)):
 
 
 @router.patch("/{note_id}/", response_model=NoteSchema, status_code=status.HTTP_200_OK)
-async def update_note(data: NoteUpdateVersionCreateSchema, note_id: uuid.UUID, db: AsyncSession = Depends(get_db)):
+async def update_note(
+        data: NoteUpdateVersionCreateSchema,
+        note_id: uuid.UUID,
+        db: AsyncSession = Depends(get_db),
+        token: str = Depends(get_token),
+        jwt_manager: JWTAuthManagerInterface = Depends(get_jwt_auth_manager),
+):
+    user_id = get_user_id_or_unauthorized(jwt_manager=jwt_manager, token=token)
+
     note = await db.get(Note, note_id)
 
     if not note:
