bc0fee10c500969daa5985a0fcc0c3eda2844c2a
roman <skyfoxwork@gmail.com>
2025-03-25 18:21:01 +0200

    add inherit Settings from BaseSettings (pydantic_settings)

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,19 +1,19 @@
 import os
 
 from dotenv import load_dotenv
-
+from pydantic_settings import BaseSettings
 
 load_dotenv()
 
 
-class Settings:
+class Settings(BaseSettings):
     POSTGRES_USER: str = os.getenv("POSTGRES_USER", "test_user")
     POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD", "test_password")
     POSTGRES_HOST: str = os.getenv("POSTGRES_HOST", "test_host")
     POSTGRES_DB_PORT: int = int(os.getenv("POSTGRES_PORT", 5432))
     POSTGRES_DB: str = os.getenv("POSTGRES_DB", "test_db")
 
-    GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+    GEMINI_API_KEY: str = os.getenv("GEMINI_API_KEY")
 
     SECRET_KEY_ACCESS: str = os.getenv("SECRET_KEY_ACCESS", os.urandom(32))
     SECRET_KEY_REFRESH: str = os.getenv("SECRET_KEY_REFRESH", os.urandom(32))
