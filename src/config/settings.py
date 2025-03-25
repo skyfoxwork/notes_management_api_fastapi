671334c1b26a0b47fcacb85c38f87f55b8df6a7b
roman <skyfoxwork@gmail.com>
2025-03-25 11:34:42 +0200

    fix src/config/settings.py postgres env name

--- src/config/settings.py
+++ src/config/settings.py
@@ -10,8 +10,8 @@ class Settings:
     POSTGRES_USER: str = os.getenv("POSTGRES_USER", "test_user")
     POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD", "test_password")
     POSTGRES_HOST: str = os.getenv("POSTGRES_HOST", "test_host")
-    POSTGRES_DB_PORT: int = int(os.getenv("POSTGRES_DB_PORT", 5432))
-    POSTGRES_DB: str = os.getenv("POSTGRES_DB_NAME", "test_db")
+    POSTGRES_DB_PORT: int = int(os.getenv("POSTGRES_PORT", 5432))
+    POSTGRES_DB: str = os.getenv("POSTGRES_DB", "test_db")
 
     GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
